791aa1681903ad1754a68af830d3b705659406b6
jounger <nguyenvanan.12a8@gmail.com>
2021-08-04 13:49:45 +0700

    fix: add menu to specific submenu

--- src/Http/Middleware/AddToMenus.php
+++ src/Http/Middleware/AddToMenus.php
@@ -1,19 +1,24 @@
 <?php
+
 namespace Jounger\ApiConnector\Http\Middleware;
 
 use Closure;
 use Lavary\Menu\Facade as Menu;
 
 
-class AddToMenus 
+class AddToMenus
 {
 
     public function handle($request, Closure $next)
     {
 
         // Add a menu option to the top to point to our page
-        $menu = Menu::get('sidebar_processes');
-        $submenu = $menu->add('Datasource');
+        $sidebar_menu = config('theme.sidebar_name', 'sidebar_processes');
+        $menu = Menu::get($sidebar_menu);
+        $submenu = $menu->get('sidebar_extension');
+        if (is_null($submenu)) {
+            $submenu = $menu->add('Tính năng mở rộng')->nickname('sidebar_extension');
+        }
 
         // Add our menu item to the top nav
         $submenu->add('API Connectors', [
@@ -24,5 +29,4 @@ class AddToMenus
 
         return $next($request);
     }
-    
 }
